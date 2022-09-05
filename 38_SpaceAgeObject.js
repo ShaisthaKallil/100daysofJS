58ce158c5999d1ee544bc648ac562e2d68907825
Shaistha Kallil <kallilshai@gmail.com>
2022-09-04 22:46:15 -0400

    Create 38_SpaceAgeObject.js

---
+++ 38_SpaceAgeObject.js
@@ -0,0 +1,40 @@
+//::::::::::::::::::::::::SPACE AGE:::::::::::::::::::::::::::::://
+///////////////////////////////////////////////////////////////////
+
+/*Instructions
+Given an age in seconds, calculate how old someone would be on:
+
+Mercury: orbital period 0.2408467 Earth years
+Venus: orbital period 0.61519726 Earth years
+Earth: orbital period 1.0 Earth years, 365.25 Earth days, or 31557600 seconds
+Mars: orbital period 1.8808158 Earth years
+Jupiter: orbital period 11.862615 Earth years
+Saturn: orbital period 29.447498 Earth years
+Uranus: orbital period 84.016846 Earth years
+Neptune: orbital period 164.79132 Earth years
+So if you were told someone were 1,000,000,000 seconds old, you should be able to say that they're 31.69 Earth-years old.*/
+
+
+const earthYearSecs = 31557600;
+
+const planetYear = {Mercury: 0.2408467,
+Venus:  0.61519726,
+Earth: 1.0,
+Mars: 1.8808158,
+Jupiter: 11.862615,
+Saturn: 29.447498,
+Uranus: 84.016846,
+Neptune: 164.79132
+};
+
+function calculateEarthYearsAge(planet,secs){
+
+	let years;
+
+  years = (secs/earthYearSecs)/planetYear[planet];
+	return years.toFixed(2);
+
+}
+
+const secondsOld = 1000000000;
+console.log(calculateEarthYearsAge('Venus',secondsOld));
